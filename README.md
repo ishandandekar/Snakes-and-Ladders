062cd3ad04df9b1fc4d71a27a85382bee3389c63
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-05-01 20:35:14 +0530

    Add complete game

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 ### Work in progress!!!
 ---
 Hello and welcome,
-This is the snakes and ladders game repository which uses graphs data structure.
+This is the snakes and ladders game repository which uses python language's inbuilt libraries, data structures and functions.
+The game takes the number of players as an input and sets up the board for the players. When the player reaches 100 he/she/they win the game!
+The game randomly generates a number between 1 and 6 including 1 and 6 both as a value of dice.
 If you want to contribute, please email **ishandandekar2002@gmail.com**
 or drop a message on discord **notNahsi _#0490_**
-
-> There is an edit in the template used; Instead of adding a relation for 1 --> 38, there is a relation for 2 --> 38 to make the players start from 1
--- game.py
+++ game.py
@@ -1,23 +1,17 @@
+import random
+import os
+import time
+import sys
+
+
 class Snakes_and_Ladders:
     def __init__(self):
         self.nbrs = {}
         self.vertices = self.nbrs.keys()
-
-    def _add_relation(self, vertex, edge):
-        if vertex not in self.vertices:
-            self.nbrs[vertex] = set()
-            self.nbrs[vertex].add(edge)
-        else:
-            self.nbrs[vertex].add(edge)
-
-    def print_edges(self, vertex):
-        print(self.nbrs[vertex])
-
-    def setup_game(self, number_of_players):
-        for i in range(1, 100, 1):
+        for i in range(0, 100, 1):
             self._add_relation(i, i+1)
         # Ladders
-        self._add_relation(2, 38)
+        self._add_relation(1, 38)
         self._add_relation(4, 14)
         self._add_relation(9, 31)
         self._add_relation(21, 42)
@@ -35,14 +29,49 @@ class Snakes_and_Ladders:
         self._add_relation(93, 73)
         self._add_relation(95, 75)
         self._add_relation(98, 79)
-
+        os.system('cls')
+        print("Hello and welcome to Snakes and Ladders")
+        time.sleep(1)
+        print("A very weird way to snakes and ladders on CLI right?")
+        time.sleep(1)
+        print("That too on game built with 'Python', I mean what a coincidence!")
+        time.sleep(1)
+        print("Nevermind, Let's start the game!\nHappy playing :D")
+        time.sleep(1)
+        self.number_of_players = int(
+            input("Enter the number of players: "))
         self.player_pos = []
-        for i in range(number_of_players):
-            self.player_pos.append(1)
+        for i in range(self.number_of_players):
+            self.player_pos.append(0)
+        print("\nLoading the game...")
+        self._start_game()
 
-    def move_player(self, number_of_jumps, player_number):
+    def _start_game(self):
+        dice = list(range(1, 7, 1))
+        printable_sentences = ["It's Player {}'s chance",
+                               "It's Player {}'s turn to play"]
+        os.system("cls")
+        print("The game board has been set-up as shown in the image")
+        from PIL import Image
+        with Image.open("Template.png") as im:
+            im.show()
+        while 100 not in self.player_pos:
+            for i in range(self.number_of_players):
+                print(random.choice(printable_sentences).format(i+1))
+                time.sleep(0.5)
+                print("Rolling the dice...")
+                number_of_jumps = random.choice(dice)
+                print(f"The dice says you to move {number_of_jumps} places")
+                self._move_player(
+                    number_of_jumps=number_of_jumps, player_number=i+1)
+
+    def _move_player(self, number_of_jumps, player_number):
         initial_position = self.player_pos[player_number-1]
         new_pos = initial_position+number_of_jumps
+        if new_pos >= 100:
+            print(
+                f"Congratulations Player {player_number}!!!\nYou won the game!!!")
+            sys.exit("Woohoo!!!\nSee you guys later")
         if len(self.nbrs[new_pos]) == 2:
             new_pos = list(self.nbrs[new_pos])[-1]
         self.player_pos[player_number-1] = new_pos
@@ -50,6 +79,7 @@ class Snakes_and_Ladders:
         if self._endgame():
             print(
                 f"Congratulations Player {player_number}!!!\nYou won the game!!!")
+            sys.exit("Woohoo!!!\nSee you guys later")
         else:
             print(f"Player {player_number} has been moved to {new_pos}")
 
@@ -58,8 +88,17 @@ class Snakes_and_Ladders:
             return True
         return False
 
+    def _add_relation(self, vertex, edge):
+        if vertex not in self.vertices:
+            self.nbrs[vertex] = set()
+            self.nbrs[vertex].add(edge)
+        else:
+            self.nbrs[vertex].add(edge)
 
+    def print_edges(self, vertex):
+        print(self.nbrs[vertex])
+
+
+# Testing the game
 if __name__ == '__main__':
-    g = Snakes_and_Ladders()
-    g.setup_game()
-    print(g.move_player(1, 1))
+    client = Snakes_and_Ladders()
